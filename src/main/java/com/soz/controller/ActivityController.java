92835ef5329e6ecfcf7abf3cdc221e381ca4a328
Melakinf <52651672+Melakinf@users.noreply.github.com>
2020-10-06 11:22:37 +0800

    update

--- src/main/java/com/soz/controller/ActivityController.java
+++ src/main/java/com/soz/controller/ActivityController.java
@@ -4,6 +4,7 @@ import com.soz.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
